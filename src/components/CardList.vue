40e794a6a8c60249fc98cc1c3fb43bcd6f24ba52
Oryntay Abylaikhan <abylay.oryntay.02@mail.ru>
2025-06-17 00:11:40 +0500

    vue

---
+++ src/components/CardList.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Card from "./Card.vue";
+
+const onClickAdd = () => {
+    alert('Добавить');
+}
+</script>
+
+<template>
+    <div class="grid grid-cols-4 gap-5">
+       <Card 
+        title="Мужские Кроссовки Nike Blazer Mid Suede" 
+        imageUrl="/sneakers/sneakers-1.jpg" 
+        :price="5000"
+        :isFavorite="false"
+        :onClickAdd="onClickAdd"
+        :isAdded="true"
+       />
+       <Card imageUrl="/sneakers/sneakers-2.jpg"/>
+       <Card imageUrl="/sneakers/sneakers-3.jpg"/>
+       <Card imageUrl="/sneakers/sneakers-4.jpg"/>
+       <Card imageUrl="/sneakers/sneakers-5.jpg"/>
+       <Card imageUrl="/sneakers/sneakers-6.jpg"/>
+       <Card imageUrl="/sneakers/sneakers-7.jpg"/>
+       <Card imageUrl="/sneakers/sneakers-8.jpg"/>
+       <Card imageUrl="/sneakers/sneakers-9.jpg"/>
+       <Card imageUrl="/sneakers/sneakers-10.jpg"/>
+       <Card imageUrl="/sneakers/sneakers-11.jpg"/>
+       <Card imageUrl="/sneakers/sneakers-12.jpg"/>
+    </div>
+</template>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <div class="flex items-center border border-slate-200 p-4 rounded-xl">
+        <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneaker"/>
+
+        <div class="flex flex-col">
+            <p>Кроссовки Nike Air Max 270</p>
+
+            <div class="flex justify-between">
+                <b>12990 руб.</b>
+                <img src="/close.svg"/>
+            </div>
+        </div>
+    </div>
+
+</template>
